83bb361c5feb895706814bc81fbca82cde3e3f45
PanovaON <olgapanova1980@bk.ru>
2026-06-25 13:53:19 +0300

    Проект успешно переведен с SQLite на PostgreSQL

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -84,11 +84,21 @@ WSGI_APPLICATION = 'NewsPaper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'news_portal_db',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
